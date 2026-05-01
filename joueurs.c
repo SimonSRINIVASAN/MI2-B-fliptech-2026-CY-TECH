e37f103cc98dccaaee35e32f29b60b9beb8a6d92
SimonSRINIVASAN <simon.srinivasan1@etu.cyu.fr>
2026-05-01 22:01:19 +0200

    Création du fichier jouerus.c

--- joueurs.c
+++ joueurs.c
@@ -1,1 +1,28 @@
-//Ici on fait tout ce qui arrive a un joueur genre ajouter une carte calculer le score etc
+#include <stdio.h>
+#include <stdlib.h>
+#include "joueurs.h"
+
+Joueur* initialiser_joueurs(int *nb_joueurs){
+
+    printf("Combien de joueurs vont participer ? ");
+    scanf("%d", nb_joueurs);
+
+    Joueur *liste_joueurs = malloc((*nb_joueurs) * sizeof(Joueur));
+
+    if (liste_joueurs ==NULL){
+        printf("Erreur : espace mémoire insuffisant.\n");
+        exit(1);
+    }
+
+    for (int i=0; i < *nb_joueurs; i++){
+        printf("Entrez le pseudo du joueur %d : ", i+1);
+        scanf("%s", liste_joueurs[i].pseudo);
+
+        liste_joueurs[i].score = 0;
+        liste_joueurs[i].nombre_cartes = 0;
+        liste_joueurs[i].etat = EN_JEU;
+    }
+    
+    return liste_joueurs;
+}
+
